b80b52bf539439389a5fc7b89fa4c5f6cdec6592
WaqarAwan376 <tr.awan.waqar@gmail.com>
2025-02-07 16:25:48 -0500

    Source: Added properties support in edge class

--- Source/utils/edges.py
+++ Source/utils/edges.py
@@ -14,16 +14,22 @@ class EdgeRelation:
 
 class Edge:
     def __init__(self, relation_name, fromNodeType,
-                 fromPropertyName, fromPropertyVal,
-                 toNodeType, toPropertyName, toPropertyVal):
+                 fromPropertyName, fromPropertyVal, toNodeType,
+                 toPropertyName, toPropertyVal, properties=None):
         self.relationName = relation_name
         self.from_ = EdgeRelation(
             fromNodeType, fromPropertyName, fromPropertyVal)
         self.to_ = EdgeRelation(toNodeType, toPropertyName, toPropertyVal)
+        if properties:
+            self.properties = properties
 
     def to_dict(self):
-        return {
+        data = {
             "relationName": self.relationName,
             "from": self.from_.to_dict(),
             "to": self.to_.to_dict(),
         }
+        if hasattr(self, "properties") and self.properties:
+            data["properties"] = self.properties
+
+        return data
